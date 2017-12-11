5a57fe1687c8a5f5cc064656764e5f3e6b7110cf
ouchaRain <34406117+ouchaRain@users.noreply.github.com>
2017-12-11 17:52:24 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,4 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.login('Mzg5ODIxMDA5NzM5NTEzODU2.DRBKaA.zxqtpgsX9-cNAz83dYdbyjxC00w');
-
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.Mzg5ODIxMDA5NzM5NTEzODU2.DRBKaA.zxqtpgsX9-cNAz83dYdbyjxC00w)
