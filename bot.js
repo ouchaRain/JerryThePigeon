fc12c33c5d6e122d4a00b2e527f142d15fb246d0
ouchaRain <34406117+ouchaRain@users.noreply.github.com>
2017-12-11 17:13:23 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.login('Mzg5ODIxMDA5NzM5NTEzODU2.DRBKaA.zxqtpgsX9-cNAz83dYdbyjxC00w');
+
+client.login(process.env.BOT_TOKEN)
